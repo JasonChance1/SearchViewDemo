a0cf62eac7b28fbeaf916852ca2397863576c85e
abc <3136966811@qq.com>
2024-02-23 16:02:23 +0800

    分支测试s

--- app/src/main/java/com/example/searchviewdemo/SearchFragment.kt
+++ app/src/main/java/com/example/searchviewdemo/SearchFragment.kt
@@ -34,7 +34,7 @@ class SearchFragment : Fragment() {
             binding.searchView.hide()
 
 
-            /**dd
+            /**ddss
              *
              */
             binding.result.text = binding.searchView.text
